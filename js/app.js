8b1343a31c0c244c7130a3e60c7638a4aa1390b7
unnati sangraula <unnatisangraula@gmail.com>
2026-01-31 20:26:57 +0545

    added edit completed function

--- js/app.js
+++ js/app.js
@@ -10,3 +10,12 @@ $(document).ready(function () {
   render();
 });
 
+function editCompleted(itemId) {
+  items = $.map(items, function (item) {
+    if (item.id === itemId) {
+      return $.extend({}, item, { completed: !item.completed });
+    }
+    return item;
+  });
+  render();
+}
